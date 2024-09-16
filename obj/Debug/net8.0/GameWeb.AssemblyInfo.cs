e99d7238fedb8847f1cce71e52e6bd53f26de0a2
조재혁 <daniel@jojaehyeogs-MacBook-Pro.local>
2024-09-16 20:07:56 +0900

    init

--- obj/Debug/net8.0/GameWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d62ed62b42d21dadd8e0224c4eefa558317c861c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
