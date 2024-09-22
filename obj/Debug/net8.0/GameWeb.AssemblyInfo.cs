a4533ff4fc4469ae30412c1e465ef73e109c64a3
조재혁 <daniel@jojaehyeogs-MacBook-Pro.local>
2024-09-22 16:51:25 +0900

    layout design added

--- obj/Debug/net8.0/GameWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e6d73ac0dda33264f9d4ed7796d113f1e4a9abe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+875337b4af39c29c4cea82299340007991988adb")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
